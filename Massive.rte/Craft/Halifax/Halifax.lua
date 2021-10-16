fd0d698a6ffab8c8f6870199330af9cb5c80efa8
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2021-10-16 17:10:53 +0300

    craft wind

--- Massive.rte/Craft/Halifax/Halifax.lua
+++ Massive.rte/Craft/Halifax/Halifax.lua
@@ -6,6 +6,11 @@ require("AI/NativeDropShipAI")
 function Create(self)
 	self.AI = NativeDropShipAI:Create(self)
 	
+	self.windLoopSound = CreateSoundContainer("WindLoop Halifax Massive", "Massive.rte");
+	self.windLoopSound.Volume = 0;
+	self.windLoopSound:Play(self.Pos);
+	
+	
 	self.terrainImpactSlowSound = CreateSoundContainer("TerrainImpactSlow Halifax Massive", "Massive.rte");
 	self.terrainImpactFastSound = CreateSoundContainer("TerrainImpactFast Halifax Massive", "Massive.rte");
 	self.plummetSound = CreateSoundContainer("Plummet Halifax Massive", "Massive.rte");
@@ -27,6 +32,30 @@ end
 
 function Update(self)
 
+	local windEndPoint = Vector(0, 0);
+	
+	if SceneMan:CastObstacleRay(self.Pos, Vector(0, 500), windEndPoint, Vector(0, 0), self.ID, self.Team, 0, 25) >= 0 then
+	
+		if self.windLoopSound.Volume < 1 then
+			self.windLoopSound.Volume = self.windLoopSound.Volume + 0.2 * TimerMan.DeltaTimeSecs;
+			if self.windLoopSound.Volume > 1 then
+				self.windLoopSound.Volume = 1;
+			end
+		end	
+	
+	else
+	
+		if self.windLoopSound.Volume > 0 then
+			self.windLoopSound.Volume = self.windLoopSound.Volume - 0.2 * TimerMan.DeltaTimeSecs;
+			if self.windLoopSound.Volume < 0 then
+				self.windLoopSound.Volume = 0;
+			end
+		end
+		
+	end
+	
+	self.windLoopSound.Pos = windEndPoint;
+
 	if self.TravelImpulse.Magnitude > 6000 and self.terrainImpactSoundTimer:IsPastSimMS(300) then
 
 		if self.TravelImpulse.Magnitude > 14000 then -- Hit
@@ -43,6 +72,7 @@ function Update(self)
 	if self.plummetSoundPlayed == false and (not self.RightEngine and not self.LeftEngine) or (self:NumberValueExists("Engine Left Failed") and self:NumberValueExists("Engine Right Failed")) then
 		self.plummetSoundPlayed = true;
 		self.plummetSound:Play(self.Pos);
+		self.windLoopSound:Stop(-1);
 	end
 
 	if self.Status > Actor.INACTIVE or self.AIMode == Actor.AIMODE_SCUTTLE then
@@ -71,8 +101,13 @@ function OnCollideWithTerrain(self)
 	end
 	
 end
-		
 
 function UpdateAI(self)
 	self.AI:Update(self)
+end
+
+function Destroy(self)
+
+	self.windLoopSound:Stop(-1);
+	
 end
